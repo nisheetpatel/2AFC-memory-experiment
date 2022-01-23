a55166253f58408b735d5dedad783074aefd6816
nisheetpatel <nisheet.pat@gmail.com>
2022-01-23 22:44:33 +0100

    reverted to original position setting temporarily for functionality

--- expt/options.py
+++ expt/options.py
@@ -4,15 +4,16 @@ from psychopy import visual
 from expt.stimuli import Stimuli
 import numpy as np
 
-x_positions = {
-    "left": -200,
-    "right": 200
-}
+# x_positions = {
+#     "left": -200,
+#     "right": 200
+# }
 
-y_positions = {
-    "center": 0,
-    "top": 120
-}
+# y_positions = {
+#     "center": 0,
+#     "top": 120
+# }
+
 
 @dataclass
 class OnScreenObject(ABC):
@@ -20,10 +21,19 @@ class OnScreenObject(ABC):
 
     win: visual.Window
     shape: visual
-    position: List[int, int]
+    # position: List[int, int]
 
-    def _set_position(self):
-        self.shape.setPos(self.position)
+    # def _set_position(self):
+    #     self.shape.setPos(self.position)
+
+    def setPosition(self, newPos):
+        # Set object position to one of left or right
+        assert newPos in ["left", "right"]
+
+        if newPos == "left":
+            self.shape.setPos([-200, 0])
+        elif newPos == "right":
+            self.shape.setPos([200, 0])
 
     def _get_position(self):
         # Fetch object position
@@ -104,14 +114,14 @@ class FeedbackText(OnScreenObject):
         if pos is not None:
             self.setPosition(newPos=pos)
 
-    # def setPosition(self, newPos):
-    #     # Set object position to one of left or right
-    #     assert newPos in ["left", "right"]
+    def setPosition(self, newPos):
+        # Set object position to one of left or right
+        assert newPos in ["left", "right"]
 
-    #     if newPos == "left":
-    #         self.shape.setPos([0, 120])
-    #     elif newPos == "right":
-    #         self.shape.setPos([400, 120])
+        if newPos == "left":
+            self.shape.setPos([0, 120])
+        elif newPos == "right":
+            self.shape.setPos([400, 120])
 
 
 @dataclass
