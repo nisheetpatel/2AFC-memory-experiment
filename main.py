22eae820cf3df5723f606049a128ab20f746a9a0
nisheetpatel <nisheet.pat@gmail.com>
2022-01-24 16:16:36 +0100

    fully functional experiment; no instructions yet

--- main.py
+++ main.py
@@ -1,43 +1,9 @@
 from pathlib import Path
-from psychopy import visual, core, data, gui
+from psychopy import visual, data
 from expt.routines import TrialRoutine
+from expt.conditions import TrialSequenceGenerator
 from expt.options import createAllOptions
-from expt.conditions import training_trial_conditions
-
-
-def define_dialog_config_window() -> gui.Dlg:
-    dialog = gui.Dlg(title="Memory Experiment")
-    dialog.addText("Subject Settings")
-    dialog.addField("Subject ID")
-    dialog.addText("Session Settings")
-    dialog.addField("Session ID")
-    dialog.addField("Session Type", choices=["practice", "training", "testing"])
-    dialog.addField(
-        "Instruction set", choices=["first session", "this ain't yo mama's first rodeo"]
-    )
-
-    ok_data = dialog.show()  # show dialog and wait for OK or Cancel
-    if dialog.OK:  # or if ok_data is not None
-        print(ok_data)
-    else:
-        print("\nUser cancelled!\n")
-        core.quit()
-
-    return dialog
-
-
-def get_info_from_config_window(dialog_window):
-    experiment_info = {
-        "Subject ID": dialog_window.data[0],
-        "Session ID": dialog_window.data[1],
-        "Session type": dialog_window.data[2],
-    }
-
-    # add date to info
-    experiment_info["dateStr"] = data.getDateStr()
-
-    return experiment_info
-
+from expt.info import define_dialog_config_window, get_info_from_config_window
 
 if __name__ == "__main__":
     # initialize info to begin session
@@ -55,10 +21,13 @@ if __name__ == "__main__":
     # create probe stimuli
     allOptions = createAllOptions(win=win)
 
+    # trial conditions
+    trial_conditions = TrialSequenceGenerator(
+        session_type=experiment_info["Session type"]
+    ).generate()
+
     # trial and experiment data handlers
-    trials = data.TrialHandlerExt(
-        trialList=training_trial_conditions, nReps=1, method="random"
-    )
+    trials = data.TrialHandler(trialList=trial_conditions, nReps=1, method="sequential")
     thisExp = data.ExperimentHandler(
         name="humanMemory",
         version="0.1",
