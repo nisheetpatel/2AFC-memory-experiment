bfe115ddbdca988b3c6b2c2370fbc05bb1f2dc4a
nisheetpatel <nisheet.pat@gmail.com>
2022-02-01 14:30:39 +0100

    updated docstrings and formatting

--- expt/info.py
+++ expt/info.py
@@ -2,6 +2,9 @@ from psychopy import core, data, gui
 
 
 def define_dialog_config_window() -> gui.Dlg:
+    """
+    Shows GUI to user to input subject and session related information.
+    """
     dialog = gui.Dlg(title="Memory Experiment")
     dialog.addText("Subject Settings")
     dialog.addField("Subject ID")
@@ -13,6 +16,7 @@ def define_dialog_config_window() -> gui.Dlg:
     )
 
     ok_data = dialog.show()  # show dialog and wait for OK or Cancel
+
     if dialog.OK:  # or if ok_data is not None
         print(ok_data)
     else:
@@ -22,7 +26,10 @@ def define_dialog_config_window() -> gui.Dlg:
     return dialog
 
 
-def get_info_from_config_window(dialog_window):
+def get_info_from_config_window(dialog_window) -> dict:
+    """
+    Extracts information input by user into the gui into a dictionary.
+    """
     experiment_info = {
         "Subject ID": dialog_window.data[0],
         "Session ID": dialog_window.data[1],
