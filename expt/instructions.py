42b9d4186493aebd3ca72f624285061d9e0e7ede
Nisheet Patel <nisheet.pat@gmail.com>
2022-04-05 14:07:41 +0300

    Added fullscreen messages to display
    at beginning, end of session/day

--- expt/instructions.py
+++ expt/instructions.py
@@ -1,28 +1,82 @@
 from dataclasses import dataclass
 from dataclasses import dataclass
-from psychopy import visual
-from pygame import font
+from psychopy import visual, event
+import numpy as np
+
+@dataclass
+class ScreenText:
+    """Fullscreen text to be displayed."""
+
+    # condition: dict
+    win: visual.Window
+    message: str
+
+    def show(self):
+        # display instructions and wait
+        message1 = visual.TextStim(
+            self.win,
+            text=self.message,
+            height=0.075,
+            units="height",
+            wrapWidth=1.25,
+        )
+        message1.draw()
+        self.win.flip()  # to show our newly drawn 'stimuli'
+
+        # pause until there's a keypress
+        _ = event.waitKeys()
+        self.win.flip()
 
 
 @dataclass
-class Instructions:
+class BeginSessionScreen(ScreenText):
+    message: str = "Press any key when ready"
+
+
+@dataclass
+class EndSessionScreen(ScreenText):
+    message: str = "Please take a break.\n Press any key when ready for the next step."
+
+
+@dataclass
+class EndOfExperimentDayScreen(ScreenText):
+    message: str = "You are done for the day.\n\nPress any key to view your earnings."
+
+
+@dataclass
+class TotalEarningsScreen:
     win: visual.Window
-    session_type: str
-    instruction_set: str
+    payoff_list: list
+    
+    def _create_text_stims(self):
+        
+        kwargs = {"win":self.win, "height":0.03, "units":"height"}
+        y = np.arange(30, -40, -6)/100
+        text_stims = []
 
-    def __post_init__(self):
-        if self.session_type == "practice":
-            instruction_text = "Practice session"
-        elif self.session_type == "training":
-            instruction_text = "Training session"
-        elif self.session_type == "testing":
-            instruction_text = "Testing session"
-        if self.instruction_set == "first session":
-            instruction_text_2 = "Instructions"
+        # show up fee
+        text_stims += [visual.TextStim(text=f"Show up fee", pos=(-0.2, 0.36), **kwargs)]
+        text_stims += [visual.TextStim(text="$25.0", pos=(0.2, 0.36), **kwargs)]
 
-        self.shape = visual.TextStim(
-            win=self.win, text=instruction_text, color="black", font="Open Sans"
-        )
-        self.shape2 = visual.TextStim(
-            win=self.win, text=instruction_text_2, color="black", font="Open Sans"
-        )
+        # session payoffs
+        for idx, payoff in enumerate(self.payoff_list):
+            text_stims += [visual.TextStim(text=f"Session {idx+1}", pos=(-0.2, y[idx]), **kwargs)]
+            text_stims += [visual.TextStim(text=f"${round(payoff, ndigits=1)}", pos=(0.2, y[idx]), **kwargs)]
+
+        # totals
+        total = round(25 + sum(self.payoff_list), 1)
+        text_stims += [visual.TextStim(text=f"Total", pos=(-0.2, -0.18), **kwargs)]
+        text_stims += [visual.TextStim(text=f"${total}", pos=(0.2, -0.18), **kwargs)]
+        
+        text_stims += [visual.TextStim(text="Press space to end experiment", pos=(0, -0.3), **kwargs)]
+
+        text_stims += [visual.Line(win=self.win, start=(0.3, -0.15), end=(-0.3, -0.15), lineWidth=0.03, color="white", units="height")]
+
+        return text_stims
+
+    def show(self):
+        text_stims = self._create_text_stims()
+        for text_stim in text_stims:
+            text_stim.draw()
+        self.win.flip()
+        event.waitKeys(keyList=["space"])
