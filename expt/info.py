3532d96b23b73488896a3096085411e2b3e7c930
Nisheet Patel <nisheet.pat@gmail.com>
2022-04-05 14:11:02 +0300

    Updated GUI, save to incorporate day routines

--- expt/info.py
+++ expt/info.py
@@ -10,11 +10,12 @@ def display_config_window() -> gui.Dlg:
     """
     # define fields to show in gui
     dialog = gui.Dlg(title="Memory Experiment")
-    dialog.addText("Subject Settings")
+    # dialog.addText("Subject Settings")
     dialog.addField("Subject ID")
-    dialog.addText("Session Settings")
-    dialog.addField("Session ID")
-    dialog.addField("Session Type", choices=["practice", "training", "testing"])
+    dialog.addField("Day")
+    # dialog.addText("Session Settings")
+    # dialog.addField("Session ID")
+    # dialog.addField("Session Type", choices=["practice", "training", "testing"])
 
     # show dialog and wait for OK or Cancel
     dialog_window_data = dialog.show()
@@ -27,34 +28,35 @@ def display_config_window() -> gui.Dlg:
     return dialog_window_data
 
 
-def get_config_info(dialog_window, set_seeds=True) -> dict:
+def get_config_info(dialog_window) -> dict:
     """
     Extracts information input by user into the gui into a dictionary.
     """
     experiment_info = {
-        "Subject ID": dialog_window[0],
-        "Session ID": dialog_window[1],
-        "Session type": dialog_window[2],
+        "Subject ID": int(dialog_window[0]),
+        "Day": int(dialog_window[1]),
+        # "Session ID": dialog_window[1],
+        # "Session type": dialog_window[2],
     }
 
-    # add date to info
-    experiment_info["dateStr"] = data.getDateStr()
-
-    # set seeds for experiment subject and session
-    if set_seeds:
-        subject_seed = int(experiment_info["Subject ID"])
-        session_seed = int(experiment_info["Session ID"]) + 10 * subject_seed
-        testing_seed = int(experiment_info["Session type"] == "testing") * 1000
-
-        random.seed(subject_seed)
-        np.random.seed(session_seed + testing_seed)
+    # # add date to info
+    # experiment_info["date"] = data.getDateStr()
 
     return experiment_info
+
+def set_random_seed(experiment_info: dict) -> None:
+    # set seeds for experiment subject (random) and session (np.random)
+    subject_seed = experiment_info["Subject ID"]
+    session_seed = experiment_info["Session ID"] + 10 * subject_seed
+    testing_seed = int(experiment_info["Session type"] == "testing") * 1000
+
+    random.seed(subject_seed)
+    np.random.seed(session_seed + testing_seed)
 
 
 def set_file_path(experiment_info: dict) -> str:
     Path("./data").mkdir(parents=True, exist_ok=True)
-    filename = "data/subj_{}_sess_{}_{}_{}".format(*experiment_info.values())
+    filename = "data/subject_{}_day_{}_{}_session_{}_{}".format(*experiment_info.values())
     return filename
 
 
